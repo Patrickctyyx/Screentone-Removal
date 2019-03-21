6cda915faa129b3e4482f5a231af34ae55edbb20
patrickcty <chengtiyanyang@gmail.com>
2019-03-21 11:49:02 +0800

    Add mask merging.

---
+++ mask_merging.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+from skimage.measure import label
+
+from remove_mask import screentone_removal
+from coherent_line_drawing.coherent_line_drawing import CoherentLineDrawing
+
+
+def get_final_mask(filename):
+    # get mask removal
+    _, i_base, mask_rm = screentone_removal(filename)
+
+    # get mask line preserving
+    w_dog = i_base - 2
+    sigma_c = 0.3 * ((w_dog - 1) * 0.5 - 1) + 0.8
+    cld = CoherentLineDrawing(sigma_c=sigma_c)
+    cld.read_src('mangas/manga4.png')
+    cld.gen_cld()
+    mask_lp = cld.result
+
+    # combine the two masks through connected components
+    mask_comb = cv2.bitwise_or(mask_rm, mask_rm, mask=mask_lp)
+    m_final = np.zeros(mask_comb.shape, mask_comb.dtype)
+
+    labeled_img, ncc = label(mask_comb, connectivity=2, return_num=True)
+    for n in range(1, ncc + 1):
+        # traverse all connected component
+        idx = np.where(labeled_img == n)
+        # if relevant area in mask removal is not all zero
+        # then set final values to 1
+        arr = mask_rm[idx]
+        none_zero_num = np.count_nonzero(arr)
+        if none_zero_num > 0:
+            m_final[idx] = 255
+
+    return m_final
